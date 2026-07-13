30b625e3c4b3109152f7fafa2c220738bcdab93d
Krutarth Raval <ravalkrutarth95@gmail.com>
2026-07-13 19:08:00 +0530

    fix: add YouTube to CSP script-src to resolve iframe_api loading and removeAttribute errors

--- cinexium/next.config.ts
+++ cinexium/next.config.ts
@@ -11,7 +11,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://apis.google.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://accounts.google.com https://apis.google.com https://www.youtube.com",
               "style-src 'self' 'unsafe-inline'",
               "img-src 'self' data: blob: https:",
               "font-src 'self' data:",
